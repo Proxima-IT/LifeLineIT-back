511a98ea9fbda042c3bfff109bc531b6dd3c6bcc
ettisafxrup <ettisafrup@gmail.com>
2025-07-31 00:47:43 +0600

    enabling rateLimit for auth

--- routes/auth.js
+++ routes/auth.js
@@ -12,23 +12,27 @@ const verifyJWT = require("@middlewares/authMiddleware")
 const rateLimiter = require("@middlewares/rateLimiter")
 
 // ROOT: /api/auth
-router.post("/otp-verify", otpVerification)
+router.post(
+  "/otp-verify",
+  rateLimiter(2 * 1000, 1, "Otp Limit Exceed! Please try again af an hour."),
+  otpVerification
+)
 
 router.post(
   "/register",
-  // rateLimiter(3600 * 1000, 3, "Otp Limit Exceed! Please try again af an hour."),
+  rateLimiter(2 * 1000, 1, "Otp Limit Exceed! Please try again af an hour."),
   registerController
 )
 
 router.post(
   "/login",
-  // rateLimiter(60 * 15 * 1000, 5, "Login limit exceed! Please try again later."),
+  rateLimiter(60 * 15 * 1000, 5, "Login limit exceed! Please try again later."),
   loginController
 )
 
 router.get(
   "/logout",
-  // rateLimiter(3600 * 2 * 1000, 4, "Please try again later."),
+  rateLimiter(60 * 15 * 1000, 5, "Please try again later."),
   logoutController
 ) // Logout Route
 
