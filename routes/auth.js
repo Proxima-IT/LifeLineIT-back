7dd8886b3874e620ca6a47d1d3c4fa45cdbc24f2
ettisafxrup <ettisafrup@gmail.com>
2025-08-03 09:25:20 +0600

    fixing rate limitings

--- routes/auth.js
+++ routes/auth.js
@@ -26,13 +26,13 @@ router.post(
 
 router.post(
   "/login",
-  rateLimiter(60 * 15 * 1000, 5, "Login limit exceed! Please try again later."),
+  rateLimiter(1 * 1000, 1, "Login limit exceed! Please try again later."),
   loginController
 )
 
 router.get(
   "/logout",
-  rateLimiter(60 * 15 * 1000, 5, "Please try again later."),
+  rateLimiter(1 * 1000, 1, "Please try again later."),
   logoutController
 ) // Logout Route
 
