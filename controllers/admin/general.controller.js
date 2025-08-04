1f85bf0f2da8d0c70ea9800ba28fab7c239c1fd3
ettisafxrup <ettisafrup@gmail.com>
2025-08-05 00:16:27 +0600

    fixing general cachings

--- controllers/admin/general.controller.js
+++ controllers/admin/general.controller.js
@@ -1,10 +1,12 @@
 const sanitize = require("mongo-sanitize")
 const General = require("@models/General")
 const { get } = require("mongoose")
+const client = require("@utils/redisClient")
 
 exports.addGeneralSettings = async (req, res) => {
   try {
     const data = sanitize(req.body)
+    const CACHE_KEY = "general"
 
     const result = await General.findOneAndUpdate(
       {},
@@ -12,7 +14,8 @@ exports.addGeneralSettings = async (req, res) => {
       { upsert: true, new: true, setDefaultsOnInsert: true } // options
     )
 
-    res.json({ message: "Success", data: result })
+    await client.set(CACHE_KEY, JSON.stringify(result), { EX: 60 })
+    return res.json({ message: "Success", data: result })
   } catch (error) {
     console.error(error)
     res.status(500).json({ message: "Error", error })
@@ -21,6 +24,11 @@ exports.addGeneralSettings = async (req, res) => {
 
 exports.getGeneralSettings = async (req, res) => {
   try {
+    const CACHE_KEY = "general"
+    const cachedCourses = await client.get(CACHE_KEY)
+
+    if (cachedCourses) return res.json(JSON.parse(cachedCourses))
+
     const getData = await General.find({}).lean()
     res.json(getData[0])
   } catch (error) {
